9c8befb07954137db50b717f8fa9f968e96ef1c1
= <=>
2022-05-02 14:09:22 +0200

    servicio usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,17 @@ import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule, UsuariosModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    UsuariosModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from '../../services/usuario.service';
 
 @Component({
   selector: 'app-lista',
   templateUrl: './lista.component.html',
-  styleUrls: ['./lista.component.css']
+  styleUrls: ['./lista.component.css'],
 })
 export class ListaComponent implements OnInit {
-
-  constructor() { }
+  constructor(private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
+    this.usuarioService
+      .getUsers()
+      .subscribe((users) => console.log(users));
   }
-
 }
