2a955f445cef1bcdb9ceb856caaefecc1aaaba1a
= <=>
2022-05-04 15:31:02 +0200

    mostramos informacion al usuario vista template

--- src/app/usuarios/usuario/usuario.component.ts
+++ src/app/usuarios/usuario/usuario.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store/app.reducer';
 import { cargarUsuario } from '../../store/actions/usuario.action';
+import { Usuario } from '../../models/usuario.models';
 
 @Component({
   selector: 'app-usuario',
@@ -10,9 +11,19 @@ import { cargarUsuario } from '../../store/actions/usuario.action';
   styleUrls: ['./usuario.component.css'],
 })
 export class UsuarioComponent implements OnInit {
+  usuario: Usuario;
+  loading = false;
+  error: any;
+
   constructor(private router: ActivatedRoute, private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.store.select('usuario').subscribe(({ user, loading, error }) => {
+      this.usuario = user;
+      this.loading = loading;
+      this.error = error;
+    });
+
     this.router.params.subscribe(({ id }) => {
       this.store.dispatch(cargarUsuario({ id }));
     });
