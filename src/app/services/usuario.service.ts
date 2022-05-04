6cdd4ad18dc66bb152124c1808bb53b157d44f4b
= <=>
2022-05-04 14:07:04 +0200

    mostrar informacion en pantalla, spinner de carga y errores por pantalla

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -6,12 +6,12 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class UsuarioService {
-  private url = 'https://reqres.in/api0928456098254';
+  private url = 'https://reqres.in/api';
   constructor(private http: HttpClient) {}
 
   getUsers() {
     return this.http
-      .get(`${this.url}/users?per_page=6`)
+      .get(`${this.url}/users?per_page=6&delay=3`)
       .pipe(map((resp) => resp['data']));
   }
 }
--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { UsuarioService } from '../../services/usuario.service';
 import { Usuario } from '../../models/usuario.models';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../store/app.reducer';
@@ -12,13 +11,17 @@ import * as UsuariosActions from '../../store/actions/usuarios.actions';
 })
 export class ListaComponent implements OnInit {
   usuarios: Usuario[] = [];
+  loading = false;
+  error: any;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.store
-      .select('usuarios')
-      .subscribe(({ users }) => (this.usuarios = users));
+    this.store.select('usuarios').subscribe(({ users, loading, error }) => {
+      this.usuarios = users;
+      this.loading = loading;
+      this.error = error;
+    });
 
     this.store.dispatch(UsuariosActions.cargarUsuarios());
 
